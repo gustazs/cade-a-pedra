1dca101c64211c09b0299da585f8d16a4d6d6a4c
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-02-18 09:42:11 -0300

    Adicionando a estrutura e estilo da página

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Qual é o problema?
+Criar um jogo em que há 3 copos, e o programa aleatoriamente vai colocar 1 pedra em um dos copos
+O objetivo do usuário é acertar em qual copo está, caso erre perde pontos, e com um limite de tentativas
+Deverá ter 3 níveis de dificuldade, fácil, médio e difícil, ou seja, de acordo com a quantidade copos maior a dificuldade.
+
+Dividir para conquistar
+Desenvolver HTML
+Desenvolver Estilo
+Como serão colocados os 3 copos
+Como fazer um programa para aleatoriamente atribuir o valor (pedra)
+Desenvolver o sistema de pontos
+Desenvolver limite de tentativas
+Criar 3 níveis de dificuldade para cada página
+
+*/
