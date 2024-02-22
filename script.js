2d50b59f943d929494d59f0be0148ea444cfac09
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-02-22 19:21:24 -0300

    Projetando a lógica do jogo em JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 Qual é o problema?
 Criar um jogo em que há 3 copos, e o programa aleatoriamente vai colocar 1 pedra em um dos copos
@@ -14,3 +15,66 @@ Desenvolver limite de tentativas
 Criar 3 níveis de dificuldade para cada página
 
 */
+
+const copoSecreto = Math.trunc(Math.random() * 3) + 1;
+let vidas = 2;
+console.log(copoSecreto);
+
+document.querySelector(".um").addEventListener('click', copoUm);
+document.querySelector(".dois").addEventListener('click', copoDois);
+document.querySelector(".tres").addEventListener('click', copoTres);
+
+function copoUm() {
+    if (copoSecreto === 1) {
+        document.querySelector(".mensagem").textContent = '✅ Você encontrou a pedra!';
+        document.querySelector("#um").src = "img/pedra-pixelart.png";
+        document.querySelector(".dois").removeEventListener('click', copoDois);
+        document.querySelector(".tres").removeEventListener('click', copoTres);
+    } else {
+        if (vidas > 1) {
+            vidas--;
+            document.querySelector('.vidas').textContent = vidas;
+            document.querySelector(".mensagem").textContent = '🚫 Copo errado!';
+        } else {
+            document.querySelector(".mensagem").textContent = '❌ Você perdeu o jogo!';
+            document.querySelector('.vidas').textContent = 0;
+            document.querySelector(".um").removeEventListener('click', copoUm);
+            document.querySelector(".dois").removeEventListener('click', copoDois);
+            document.querySelector(".tres").removeEventListener('click', copoTres);
+        }
+    }
+}
+
+function copoDois() {
+    if (copoSecreto === 2) {
+        document.querySelector(".mensagem").textContent = '✅ Você encontrou a pedra!';
+        document.querySelector("#dois").src = "img/pedra-pixelart.png";
+    } else {
+        if (vidas > 1) {
+            vidas--;
+            document.querySelector('.vidas').textContent = vidas;
+            document.querySelector(".mensagem").textContent = '🚫 Copo errado!';
+        } else {
+            document.querySelector(".mensagem").textContent = '❌ Você perdeu o jogo!';
+            document.querySelector('.vidas').textContent = 0;
+        }
+    }
+}
+
+function copoTres() {
+    if (copoSecreto === 3) {
+        document.querySelector(".mensagem").textContent = '✅ Você encontrou a pedra!';
+        document.querySelector("#tres").src = "img/pedra-pixelart.png";
+
+    } else {
+        if (vidas > 1) {
+            vidas--;
+            document.querySelector('.vidas').textContent = vidas;
+            document.querySelector(".mensagem").textContent = '🚫 Copo errado!';
+        } else {
+            document.querySelector(".mensagem").textContent = '❌ Você perdeu o jogo!';
+            document.querySelector('.vidas').textContent = 0;
+        }
+    }
+}
+
